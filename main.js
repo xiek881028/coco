680e13748dd22645a082a2bbf6697e40a1612f73
xiek <xk285985285@qq.com>
2017-04-07 14:48:21 +0800

    add font-awesome

--- main.js
+++ main.js
@@ -12,27 +12,28 @@ function createWindow () {
 		width: 400,
 		height: 600,
 		frame: false,
+		title: "八嘎猪",
 		// transparent: true
-})
+	})
 	// win.loadURL('http://bagazhu.com')
 
 	// 加载应用的 index.html。
-win.loadURL(url.format({
-	pathname: path.join(__dirname, 'view/index.html'),
-	protocol: 'file:',
-	slashes: true
-}))
+	win.loadURL(url.format({
+		pathname: path.join(__dirname, 'view/index.html'),
+		protocol: 'file:',
+		slashes: true
+	}))
 
-// 打开开发者工具。
-	//win.webContents.openDevTools()
+	// 打开开发者工具。
+	win.webContents.openDevTools()
 
-// 当 window 被关闭，这个事件会被触发。
-win.on('closed', () => {
-	// 取消引用 window 对象，如果你的应用支持多窗口的话，
-	// 通常会把多个 window 对象存放在一个数组里面，
-	// 与此同时，你应该删除相应的元素。
-	win = null
-  })
+	// 当 window 被关闭，这个事件会被触发。
+	win.on('closed', () => {
+		// 取消引用 window 对象，如果你的应用支持多窗口的话，
+		// 通常会把多个 window 对象存放在一个数组里面，
+		// 与此同时，你应该删除相应的元素。
+		win = null
+	})
 }
 
 // Electron 会在初始化后并准备
